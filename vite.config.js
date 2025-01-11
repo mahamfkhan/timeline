80bb2d891c0aed81a17c0b8c122e56c1f7612584
Maham Faisal Khan <34014138+mahamfkhan@users.noreply.github.com>
2025-01-11 16:40:31 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',  // Replace with your repository name
+  base: '/timeline/',  // Replace with your repository name
 })
