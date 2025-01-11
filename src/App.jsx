0929b926bf52d480d2706aa7db5918f6f6dd634e
Maham Faisal Khan <34014138+mahamfkhan@users.noreply.github.com>
2025-01-11 16:49:13 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import BookTimeline from './components/BookTimeline'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <BookTimeline />
+    </div>
+  )
+}
+
+export default App
